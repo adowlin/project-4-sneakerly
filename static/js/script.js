a962a6faef2a054f363372b06175f28a5c138d0e
adowlin <dowlingalison@gmail.com>
2021-08-08 18:38:13 +0000

    Add CSS & JS for navbar skew on scroll

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+// JS for navbar animation on scroll adapted from: https://codepen.io/rauldronca/pen/yQrmeE
+const navShape = document.getElementById('main-nav-shape');
+const imgShape = document.getElementById('hero-image-shape');
+
+let currentPos = window.pageYOffset;
+
+const update = () => {
+	const newPos = window.pageYOffset;
+	const diff = newPos - currentPos;
+	let deg = 0;
+
+    if (newPos > 450) {
+        deg = 0;
+    } else {
+        deg = -10;
+    }
+	
+	navShape.style.transform = `skewY(${ deg }deg)`;
+    imgShape.style.transform = `skewY(${ deg }deg)`;
+	
+	currentPos = newPos;
+	
+	requestAnimationFrame(update);
+}
+
+update();
