6d076bde218cfca60e3e225351761c57e3f05e1f
adowlin <dowlingalison@gmail.com>
2021-08-11 23:49:16 +0000

    Add model methods to Checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,15 +1,15 @@
 import uuid
 
 from django.db import models
-from django.db.models import Sum
-from django.conf import settings
+
+from django_countries.fields import CountryField
 
 from products.models import Product
 
 
 class Booking(models.Model):
-    booking_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+    booking_number = models.CharField(
+        max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -21,13 +21,57 @@ class Booking(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     order_date = models.DateTimeField(auto_now_add=True)
     start_date = models.DateTimeField(auto_now_add=True)
-    total_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    rental_days = models.IntegerField(null=False, blank=False, default=0)
+    product_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    total_cost = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
+
+    def _generate_booking_number(self):
+        """
+        Generate a random, unique booking number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def calculate_total(self):
+        """
+        Update grand total for each rental day.
+        """
+        self.total_cost = self.product_price * self.rental_days
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the booking number
+        if it hasn't been set already.
+        """
+        if not self.booking_number:
+            self.booking_number = self._generate_booking_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.booking_number
 
 
 class BookingLineItem(models.Model):
-    booking = models.ForeignKey(Booking, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    booking = models.ForeignKey(
+        Booking, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=4, null=True, blank=True)
     rental_days = models.IntegerField(null=False, blank=False, default=0)
-    booking_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    booking_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the booking total
+        and update the order total.
+        """
+        self.booking_total = self.product.price * self.rental_days
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on booking {self.booking.booking_number}'
