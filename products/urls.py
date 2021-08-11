5177d4ac7e2b04574af8d40e5987950b850fd453
adowlin <dowlingalison@gmail.com>
2021-08-11 00:20:29 +0000

    Add product rental page view & template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_rental, name='product_rental'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ View to return 'All Sneakers' page """
+    """ View to show 'All Sneakers' page """
 
     products = Product.objects.all()
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_rental(request, product_id):
+    """ View to show product rental booking page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_rental.html', context)
