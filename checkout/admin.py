445d8c166d4e7cb8c482c6f1ed4cf8e1c9f4af3f
adowlin <dowlingalison@gmail.com>
2021-08-12 00:12:52 +0000

    Register admin for Checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Booking, BookingLineItem
 
-# Register your models here.
+
+class BookingLineItemAdminInline(admin.TabularInline):
+    model = BookingLineItem
+    readonly_fields = ('booking_total',)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    inlines = (BookingLineItemAdminInline,)
+
+    readonly_fields = ('booking_number', 'order_date', 'total_cost',)
+    fields = ('booking_number', 'order_date', 'full_name',
+                'email', 'phone_number', 'country',
+                'postcode', 'town_or_city', 'street_address1',
+                'street_address2', 'county', 'total_cost',)
+
+    list_display = ('booking_number', 'order_date', 'full_name',
+                    'total_cost',)
+
+    ordering = ('-order_date',)
+
+
+admin.site.register(Booking, BookingAdmin)
