36fff9f8c7fe9986055ce467f934ce27a9034a4e
adowlin <dowlingalison@gmail.com>
2021-08-21 22:30:58 +0000

    Add delivery info update form to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+    bookings = profile.bookings.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'bookings': bookings,
     }
 
     return render(request, template, context)
