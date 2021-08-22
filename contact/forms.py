5333e2eb9b43a012c797fee6e45e0280c343543c
adowlin <dowlingalison@gmail.com>
2021-08-22 23:21:15 +0000

    Style contact form input elements

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,9 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ('email', 'subject', 'message',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'contact-style-input'
