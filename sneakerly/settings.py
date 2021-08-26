2b1c4d03ab8b831b4c9ba69d39f276487eca19ec
adowlin <dowlingalison@gmail.com>
2021-08-26 22:57:05 +0000

    Configure AWS settings

--- sneakerly/settings.py
+++ sneakerly/settings.py
@@ -179,6 +179,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ms4-sneakerly'
     AWS_S3_REGION_NAME = 'eu-west-1'
