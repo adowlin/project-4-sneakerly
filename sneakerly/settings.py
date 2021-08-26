de9d84256d3e860c5ca8e23cb45259d4dfd103a4
adowlin <dowlingalison@gmail.com>
2021-08-26 22:28:37 +0000

    Configure AWS settings

--- sneakerly/settings.py
+++ sneakerly/settings.py
@@ -187,7 +187,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorages'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
