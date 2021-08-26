b8ac0254a5fae9978078bca0cb272a6520671f04
adowlin <dowlingalison@gmail.com>
2021-08-26 21:09:54 +0000

    Rolled Django secret_key & set DEBUG conditional check

--- sneakerly/settings.py
+++ sneakerly/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vf*hgw)k!x+yp@&8-+=9erebef%n#2##lnn$z(z+_n)m5v5!zq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-sneakerly.herokuapp.com', 'localhost']
-
 
 # Application definition
 
