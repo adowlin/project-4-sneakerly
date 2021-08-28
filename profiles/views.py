4f66a5d16953ca9cc695315ae723c24ad1eb41dc
adowlin <dowlingalison@gmail.com>
2021-08-28 12:08:37 +0000

    Fix profile default delivey info form functionality to ensure form is saved & autofilled correctly

--- profiles/views.py
+++ profiles/views.py
@@ -12,8 +12,41 @@ from checkout.models import Booking
 def profile(request):
     """ Display user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(instance=profile)
     bookings = profile.bookings.all()
+
+    if request.method == "POST":
+        profile = UserProfile.objects.get(user=request.user)
+
+        profile_data = {
+            'default_full_name': request.POST['default_full_name'],
+            'default_phone_number': request.POST['default_phone_number'],
+            'default_postcode': request.POST['default_postcode'],
+            'default_town_or_city': request.POST['default_town_or_city'],
+            'default_street_address1': request.POST[
+                'default_street_address1'],
+            'default_street_address2': request.POST[
+                'default_street_address2'],
+            'default_county': request.POST['default_county'],
+        }
+        form = UserProfileForm(profile_data, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Information Updated Successfully!!')
+    else:
+        try:
+            profile_data = {
+                'full_name': profile.default_full_name,
+                'email': profile.user.email,
+                'phone_number': profile.default_phone_number,
+                'postcode': profile.default_postcode,
+                'town_or_city': profile.default_town_or_city,
+                'street_address1': profile.default_street_address1,
+                'street_address2': profile.default_street_address2,
+                'county': profile.default_county,
+            }
+            form = UserProfileForm(initial=profile_data, instance=profile)
+        except UserProfile.DoesNotExist:
+            form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
