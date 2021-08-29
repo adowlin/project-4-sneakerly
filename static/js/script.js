a27e5124a6cc6be135376a38d91cccc5d1935a5b
adowlin <dowlingalison@gmail.com>
2021-08-29 13:51:01 +0000

    Refactor CSS & JS files

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 // JS for navbar animation on scroll adapted from: https://codepen.io/rauldronca/pen/yQrmeE
-const navShape = document.getElementById('main-nav-shape');
-const heroShape = document.getElementById('hero-image-shape');
-const headerShape = document.getElementsByClassName('head-image-shape')[0];
+var navShape = document.getElementById('main-nav-shape');
+var heroShape = document.getElementById('hero-image-shape');
+var headerShape = document.getElementsByClassName('head-image-shape')[0];
 
 let currentPos = window.pageYOffset;
 
-const update = () => {
-	const newPos = window.pageYOffset;
-	const diff = newPos - currentPos;
+var update = () => {
+	var newPos = window.pageYOffset;
+	var diff = newPos - currentPos;
 	let deg = 0;
 
     if (newPos > 450) {
@@ -27,6 +27,6 @@ const update = () => {
 	currentPos = newPos;
 	
 	requestAnimationFrame(update);
-}
+};
 
 update();
